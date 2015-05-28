3123defa87d8e653f6ff4d19653cc4d8749bf89f
higunwoo <higunwoo@gmail.com>
2015-05-27 22:39:48 -0400

    ex5,6,7

---
+++ higunwoo/ex5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*this is a comment*/
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+	//this is alos a comment
+	printf("you are %d miles away.\n", distance);
+	return 0;
+}
